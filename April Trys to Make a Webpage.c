ee00dc4b4b7acf0ec4337405df36c4c5f53ccc3c
April <32551551+altcoded@users.noreply.github.com>
2017-10-26 22:46:27 +0100

    Add files via upload

---
+++ April Trys to Make a Webpage.c
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+
+<head>
+<title> April Lauren Ryan </title>
+</head>
+<body>
+<h1> Just a budding journalist learning to code </h1>
+<p> As I am sure you have been clever enough to guess, my name is April.  I am relatively new when it comes to the coding game, but journalism is my talent.  I am currently getting to grips to the grammatical behemoths known as HTML, CSS, JavaScript and Python </p> 
+</body>
+</html>
